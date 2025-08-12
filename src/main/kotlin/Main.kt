eb629a13abb414f063253cad0c6811a801737681
Ayfri <pierre.ayfri@gmail.com>
2025-08-12 10:21:30 +0200

    chore(main): Replace `kotlin.io.path` with `kotlinx-io.files` and ensure output directory creation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,15 @@
 import io.github.ayfri.kore.commands.say
 import io.github.ayfri.kore.dataPack
 import io.github.ayfri.kore.functions.function
-import kotlin.io.path.Path
-import kotlin.io.path.createDirectories
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
 
 const val outputPathFolder = "./out"
 
 fun main() {
 	val myDatapack = dataPack("my_datapack") {
-		path = Path(outputPathFolder).createDirectories()
+		path = Path(outputPathFolder) // Set the output path for the datapack
+		SystemFileSystem.createDirectories(path) // Create the output directory if it doesn't exist
 
 		function("my_function") {
 			say("Hello Minecraft world !")
