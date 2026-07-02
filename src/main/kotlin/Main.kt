2615b8880dce31f7249f84c6ecf87332cf8cb862
Ayfri <pierre.ayfri@gmail.com>
2026-07-02 20:49:33 +0200

    chore(main): Remove `kotlinx-io.files` dependency usage, simplify output directory handling.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,12 @@
 import io.github.ayfri.kore.commands.say
 import io.github.ayfri.kore.dataPack
 import io.github.ayfri.kore.functions.function
-import kotlinx.io.files.Path
-import kotlinx.io.files.SystemFileSystem
 
 const val outputPathFolder = "./out"
 
 fun main() {
 	val myDatapack = dataPack("my_datapack") {
-		path = Path(outputPathFolder) // Set the output path for the datapack
-		SystemFileSystem.createDirectories(path) // Create the output directory if it doesn't exist
+		path(outputPathFolder) // Set the output path for the datapack
 
 		function("my_function") {
 			say("Hello Minecraft world !")
